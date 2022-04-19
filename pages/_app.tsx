7aaa7a0ca222adb1791b175dee856f68b1c3cda7
eric1123 <eric1123@naver.com>
2022-04-19 17:04:26 +0900

    Feat/Component/pagination

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../component/Layout";
+// import Layout from "../component/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    // <Layout>
+    <Component {...pageProps} />
+    // </Layout>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import Router, { withRouter } from "next/router";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.scss";
-import React from "react";
 import SearchLocation from "../component/SearchLocation";
-import TestModals from "../component/testModal";
-
-const Home: NextPage = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.search}>
-        <SearchLocation />
-        <TestModals />
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+//import TestModals from "../component/testModal";
+// const Home: NextPage = () => {
+//   return (
+//     <div className={styles.wrapper}>
+//       <div className={styles.search}>
+//         <SearchLocation />
+//         <TestModals />
+//       </div>
+//     </div>
+//   );
+// };
+// export default Home;
